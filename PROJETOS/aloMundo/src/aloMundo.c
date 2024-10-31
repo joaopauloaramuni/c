47c76002d20b45c11f327186231a2ed4ff978f36
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-10-31 20:34:05 -0300

    Add files via upload

---
+++ PROJETOS/aloMundo/src/aloMundo.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : aloMundo.c
+ Author      : João Paulo Aramuni
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!! Hello World !!!"); /* prints !!! Hello World !!! */
+	return EXIT_SUCCESS;
+}
+
+
+
---
+++ PROJETOS/aloMundo2/src/aloMundo2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : aloMundo2.c
+ Author      : João Paulo Aramuni
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!! Alo Mundo 2 !!!"); /* prints !!! Alo Mundo !!! */
+	return EXIT_SUCCESS;
+}
